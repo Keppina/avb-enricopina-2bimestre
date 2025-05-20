02aa9f581517e30224ae12641f49be848d8e0f23
enricopina <enricopina>
2025-05-20 11:35:31 -0300

    finalização av1

---
+++ src/pages/Detalhes/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CatFact from '../../components/CatFact';
+
+const Detalhes = () => {
+  return <CatFact />;
+};
+
+export default Detalhes;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white text-center">
+      <div>
+        <h1 className="text-4xl font-bold mb-4">Bem-vindo ao site de Gatos!</h1>
+        <p className="text-sm text-purple-300">
+          Aqui você pode ver curiosidades sobre gatos, detalhes e seus favoritos.<br />
+          Use o menu acima para navegar entre as páginas!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
